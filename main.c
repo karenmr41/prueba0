7b71acc1b910c1fce7f9389a1155260db6462c91
karenmr41 <karenmr41@hotmail.com>
2022-05-10 16:49:06 -0300

    probandoPruebaCero

---
+++ main.c
@@ -0,0 +1,21 @@
+/* mostrar dos columnas con los valores equivalentes entre F° y C° */
+// int main es la funcion principal. Returnr 0 le devuelve un cero al sistema operativo
+// int a -> reserva un lugar en la memoria
+// -> precedencia en las operaciones con el cero <-
+// print f significa imprimir formato
+// \t tabulado
+// secuencia repetitiva: bucle
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float f;
+    int c;
+    c=0;
+
+    f=9*c/5+32;
+    printf("\t%f\n",f);
+    return 0;
+}
